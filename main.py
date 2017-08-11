86367b95ecfa02ee1b092e43cc550f8f509fc293
Anderson Carvalho Martins <anderson.cvlh@gmail.com>
2017-08-10 21:48:40 -0300

    Robo 2, movimentos

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import robo3d
 
 robo1 = robo.Robo(2,2)
 print(type(robo1))
-print(robo1.x)
-print(robo1.y)
+print('X igual a %s' % robo1.x)
+print('Y igual a %s' % robo1.y)
 
 robo3d = robo3d.Robo3D(5, 5, 10)
 print(type(robo3d))
-print(robo3d.x)
-print(robo3d.y)
-print(robo3d.z)
+print('X igual a %s' % robo3d.x)
+print('Y igual a %s' % robo3d.y)
+print('Z igual a %s' % robo3d.z)
+
+robo3d.move_down()
+print('Y igual a %s' % robo3d.y)
--- robo.py
+++ robo.py
@@ -2,3 +2,29 @@ class Robo():
         def __init__(self, x, y):
             self.x = x
             self.y = y
+
+        
+        def move_up(self):
+            if self.y < 10:
+                self.y =  self.y + 1
+            else:
+                print('Movimento Proibido')
+                
+        
+        def move_down(self):
+            if self.y > 0:
+                self.y = self.y - 1
+            else:
+                print('Movimento Proibido')
+
+        def move_right(self):
+            if self.x < 10:
+                self.x = self.x + 1
+            else:
+                print('Movimento Proibido')
+
+        def move_left(self):
+            if self.x > 0:
+                self.x = self.x - 1
+            else:
+                print('Movimento Proibido')
