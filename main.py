4f1c121b79fcca2c5716e3c0d524794a6704ab68
Anderson Carvalho Martins <anderson.cvlh@gmail.com>
2017-08-11 05:41:45 -0300

    Percorrendo o mapa e achando a recompensa

--- main.py
+++ main.py
@@ -2,19 +2,42 @@ import robo
 import robo3d
 import reward
 
+def check_reward(robot, rewards):
+    ok = False
+    for reward in rewards:
+        if reward.x == robot.x and reward.y == robot.y:
+            print('O robo achou a recompensa %s' % reward.name)
+            ok = True
+    return ok
 
-robo1 = robo.Robo(2,2)
+rew1 = reward.Reward(5, 5, 'Moeda')
+rew2 = reward.Reward(2, 2, 'Gasolina')
+rewardList = [rew1, rew2]
+
+robo1 = robo.Robo(5,7)
 print(type(robo1))
 print('X igual a %s' % robo1.x)
 print('Y igual a %s' % robo1.y)
 
-robo3d = robo3d.Robo3D(5, 5, 10)
-print(type(robo3d))
-print('X igual a %s' % robo3d.x)
-print('Y igual a %s' % robo3d.y)
-print('Z igual a %s' % robo3d.z)
+robo2 = robo3d.Robo3D(5, 5, 10)
+print(type(robo2))
+print('X igual a %s' % robo2.x)
+print('Y igual a %s' % robo2.y)
+print('Z igual a %s' % robo2.z)
 
-robo3d.move_down()
-print('Y igual a %s' % robo3d.y)
+robo1.move_down()
+robo1.move_down()
+check_reward(robo1, rewardList)
 
-reward = reward.Reward(5, 5, 'Recompensa')
+robo1.move_down()
+robo1.move_down()
+robo1.move_down()
+robo1.move_left()
+robo1.move_left()
+robo1.move_left()
+check_reward(robo1, rewardList)
+
+check_reward(robo2, rewardList)
+
+
+
